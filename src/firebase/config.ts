8dfaca833bd372bd6ab8382015180f15cc404551
Jariz Baguio <jariz67baguio@gmail.com>
2026-03-08 17:49:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1894857571-5c8cf",
+  "appId": "1:820207157057:web:da7754a707bbca54baabc0",
+  "apiKey": "AIzaSyBs0uWHGAFSIuRUmOZIMq3k4fwabYc2TJU",
+  "authDomain": "studio-1894857571-5c8cf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "820207157057"
+};
