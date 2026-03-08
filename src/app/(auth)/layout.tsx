643c91d0e7065a158b6579d3bafc969ce596bb9a
Jariz Baguio <jariz67baguio@gmail.com>
2026-03-08 17:07:00 +0000

    ts not centered

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="container grid min-h-[calc(100vh-8rem)] place-items-center py-10">
+    <div className="grid min-h-[calc(100vh-8rem)] place-items-center px-4 py-10">
       {children}
     </div>
   );
